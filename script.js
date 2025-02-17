3ffb5e490024b27a6a1e5d7d3f61e5d0c6e976e4
M3galodn81 <dalisdism0@gmail.com>
2025-02-17 22:34:59 +0800

    Separated 4 sections into their own page

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 import '@picocss/pico/css/pico.min.css';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    document.body.style.paddingTop = header.offsetHeight + "px";
+});
